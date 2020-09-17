4e2f1594fe7019f41ace868da85a4d7f04d32782
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-17 09:01:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function BoardMember(name, homeState, training){
   this.training=training;
   this.veto=()=>{return "You can do that!";};
   this.approve=()=>{return "No, I must disagree";};
-  this.veto=()=>{return "No, I must disagree";};
+  this.doCharity=()=>{return "No, I must disagree";};
   this.veto=()=>{return "No, I must disagree";};
   this.veto=()=>{return "No, I must disagree";};
 }
