111a16bfa35212f24cf7c60254f7b2b152f05214
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-17 09:02:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function BoardMember(name, homeState, training){
   this.approve=()=>{return "No, I must disagree";};
   this.doCharity=()=>{return "I like to help people.";};
   this.releasePressStatement =()=>{return "will see great things from Scuber.";};
-  this.sayHi =()=>{return "No, I must disagree";};
+  this.sayHi =()=>{return `Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.';};
 }
